ed42e66587c5ed97ed676dba778fb954a0d76f1d
byron <bgguerra@scl.edu.gt>
2023-07-27 07:55:20 -0600

    1ra prueba

---
+++ js/data.js
@@ -0,0 +1,119 @@
+export let data = [
+    {
+        titulo: "Cambio de pasta termica",
+        img : "https://th.bing.com/th/id/OIP.w2ngmOd8SPdGE_IM5puYhAHaHa?pid=ImgDet&rs=1",
+        descripcion : "Como cambiar la pasta termica del procesador",
+        herramientas : [
+            "Destornillador de estrella",
+            "Paño de micro fibra",
+            "Pasta termica"
+        ],
+        pasos: [
+            "Desmontar el disipador",
+            "Limpiar la vieja pasta termica",
+            "Aplicar la nueva pasta termica",
+            "Volver a montar el disipador"
+        ]
+    },
+    {
+        titulo: "Instalación de disco duro",
+        img : " ",
+        descripcion : "Proceso para instalar un nuevo disco duro en tu computadora",
+        herramientas : [
+            "Destornillador de estrella",
+            "Caja de disco duro",
+            "Disco duro"
+        ],
+        extras: ["Cable SATA", "Cable de alimentación"],
+        pasos: [
+            "Abrir la caja del computador",
+            "Colocar el disco duro en la caja",
+            "Conectar los cables SATA y de alimentación",
+            "Cerrar la caja"
+        ]
+    },
+    {
+        titulo: "Instalación de tarjeta de video",
+        img : " ",
+        descripcion : "Pasos para instalar una tarjeta de video",
+        herramientas : [
+            "Destornillador de estrella",
+            "Tarjeta de video"
+        ],
+        extras: ["Cables PCI-E si son necesarios"],
+        pasos: [
+            "Abrir la caja del computador",
+            "Localizar la ranura PCIe",
+            "Insertar la tarjeta de video",
+            "Conectar los cables PCI-E si son necesarios",
+            "Cerrar la caja"
+        ]
+    },
+    {
+        titulo: "Cambio de fuente de poder",
+        img : " ",
+        descripcion : "Como cambiar la fuente de poder de tu computadora",
+        herramientas : [
+            "Destornillador de estrella",
+            "Fuente de poder"
+        ],
+        extras: ["Cables de la fuente de poder"],
+        pasos: [
+            "Abrir la caja del computador",
+            "Desconectar la fuente de poder vieja",
+            "Instalar la nueva fuente de poder",
+            "Conectar los cables de la nueva fuente de poder",
+            "Cerrar la caja"
+        ]
+    },
+    {
+        titulo: "Instalación de tarjeta madre",
+        img : " ",
+        descripcion : "Pasos para instalar una nueva tarjeta madre",
+        herramientas : [
+            "Destornillador de estrella",
+            "Tarjeta madre"
+        ],
+        extras: ["Chasis", "Cables de conexión"],
+        pasos: [
+            "Abrir la caja del computador",
+            "Retirar la vieja tarjeta madre",
+            "Colocar la nueva tarjeta madre en el chasis",
+            "Conectar los cables de conexión",
+            "Cerrar la caja"
+        ]
+    },
+    {
+        titulo: "Instalación de RAM",
+        img : " ",
+        descripcion : "Como instalar memoria RAM en tu computadora",
+        herramientas : [
+            "Memoria RAM"
+        ],
+        extras: ["Guía del usuario de la tarjeta madre"],
+        pasos: [
+            "Abrir la caja del computador",
+            "Localizar los slots de RAM",
+            "Insertar la memoria RAM en los slots",
+            "Cerrar la caja"
+        ]
+    },
+    {
+        titulo: "Limpieza de ventiladores",
+        img : " ",
+        descripcion : "Como limpiar los ventiladores de tu computadora para mejorar su rendimiento",
+        herramientas : [
+            "Aire comprimido",
+            "Paño de microfibra"
+        ],
+        extras: ["Destornillador de estrella si es necesario"],
+        pasos: [
+            "Apagar y desconectar el computador",
+            "Abrir la caja del computador",
+            "Usar aire comprimido para quitar el polvo",
+            "Usar un paño de microfibra para limpiar",
+            "Cerrar la caja"
+        ]
+    }
+]
+
---
+++ js/header.js
@@ -0,0 +1,14 @@
+let header_DOM = document.querySelector("#header");
+
+let header_componente = 
+`
+    <h2> Tecnico Byron </h2>
+    <ul>
+        <li>titulo </li>
+        <li>titulo </li>
+        <li>titulo </li>
+        <li>titulo </li>
+    </ul>
+`;
+
+header_DOM.innerHTML=header_componente;
